b5dac4a88bc748c9dcdaf6129077e6b880fa8aa7
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-05 19:00:22 -0400

    :sparkles: - aula 04: Formas de mostrar (Output)

---
+++ aula04/script.js
@@ -0,0 +1,13 @@
+// Formas de mostrar dados atraves do documento
+//Manipulando um elemento HTML
+document.getElementById("titulo").innerHTML = "Opa, tudo bem?"
+//Criando um elemento HTML
+document.write("Algum texto")
+
+
+// Mostrar dados atraves da janela
+window.alert("Mensagem de exemplo.")
+
+
+// Mostrar dados no console
+console.log("A mensagem que eu inserir aqui aparecerá no console.")
