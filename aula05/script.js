cdec46292f8841781908324ead1c37d2e5020c36
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-05 19:23:30 -0400

    :sparkles: - aula 05: Variáveis (1/2)

---
+++ aula05/script.js
@@ -0,0 +1,42 @@
+/*
+var nome = "Hantonny"
+var idade = 26
+
+alert(nome)
+*/
+
+/*
+var x = 10
+var y = 15
+
+var total = x + y
+
+alert(total)
+*/
+
+/*
+var nome = "Hantonny" // STRING
+var sobrenome = "Korrea da Silva dos Santos"
+
+// Concatenar
+var nomecompleto = nome + " " + sobrenome
+
+alert(nomecompleto)
+*/
+
+/*
+var nome = "Hantonny"
+var idade = 26
+
+var total = nome + idade
+
+alert(total)
+*/
+
+// const x = 10 - Quando é uma constante, não é possivel mudar o seu valor.
+var x = "10"
+var y = "15"
+
+var total = x + y
+
+alert(total)
