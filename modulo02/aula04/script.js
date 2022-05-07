e3e53c1c57e9e5389fae9d5504436a0851fd92ad
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-07 19:55:37 -0400

    :sparkles: - modulo 02:aula 04: Selecionando com querySelector

---
+++ modulo02/aula04/script.js
@@ -0,0 +1,6 @@
+document.querySelector('#exemplo')
+document.querySelector('.lista')
+document.querySelectorAll('.lista')
+document.querySelector('button')
+document.querySelector('button').innerHTML = "Alguma coisa"
+document.querySelectorAll('.lista')[1].innerHTML = "Alterado"
