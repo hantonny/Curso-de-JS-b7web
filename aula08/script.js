2676f25f9f2a4221377287ebd6759ae453460cb8
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-07 19:10:00 -0400

    :sparkles: - aula 08: Variáveis (2/2)

---
+++ aula08/script.js
@@ -0,0 +1,21 @@
+//ESCOPO GERAL DA TELA
+/*
+var nome = "Hantonny"
+
+console.log(window.nome)
+*/
+
+//ESCOPO ESPECIFICO
+/*
+let nome = "Hantonny"
+
+if(nome == "Hantonny"){
+    let idade = 26;
+    console.log(idade)
+}
+*/
+
+// VALOR CONSTANTE
+const nome = "Hantonny"
+
+console.log(nome)
