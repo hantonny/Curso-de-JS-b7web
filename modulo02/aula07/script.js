e30cab78a83e67f542714fa009b8b43ac5bb3747
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-14 19:21:03 -0400

    :sparkles: - modulo 02:aula 07: Tipos de dados

---
+++ modulo02/aula07/script.js
@@ -0,0 +1,8 @@
+var nome = "Hantonny" // String
+var idade = 26 // Number
+var salvo = true // Boolean
+var cidade; // Undefined
+
+var tipo = typeof nome // typeof pega tipo da variavel
+
+console.log(nome)
