ad5c86b825d6171193e1f2c6fa3bc30ec3eebcfc
Hantonny Korrea <hantonny.20@gmail.com>
2022-05-20 23:25:04 -0400

    :sparkles: - modulo 02:aula 12: Dimensões

---
+++ modulo02/aula12/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".texto").offsetWidth
+document.querySelector(".texto").offsetHeight
+document.querySelector(".texto").scrollWidth
+document.querySelector(".texto").scrollHeight
